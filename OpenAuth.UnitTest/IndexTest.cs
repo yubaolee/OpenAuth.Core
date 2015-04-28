91384a5447e06eea5ddec9d7a3e97ccf7ba7b419
yubaolee <01211987>
2015-04-28 18:17:04 +0800

    修改登录界面

---
+++ OpenAuth.UnitTest/IndexTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenAuth.Domain.Model;
+
+namespace OpenAuth.UnitTest
+{
+    [TestClass]
+    public class IndexTest
+    {
+        [TestMethod]
+        public void MenuTest()
+        {
+            string userid = "";
+            IndexApp app = new IndexApp();
+            MenuResponse response = app.LoadMenu(userid);
+        }
+    }
+}

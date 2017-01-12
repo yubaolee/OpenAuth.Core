a1765f37ba225c8ae08f77d76c298a432ac1ec78
yubaolee <yubaolee@163.com>
2017-01-12 19:25:41 +0800

    flow ui

---
+++ OpenAuth.UnitTest/TestWorkflow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenAuth.Domain.Service;
+
+namespace OpenAuth.UnitTest
+{
+    [TestClass]
+    public class TestWorkflow :TestBase
+    {
+        private WorkflowService _app;
+
+        public TestWorkflow()
+        {
+            _app = AutofacExt.GetFromFac<WorkflowService>();
+        }
+        [TestMethod]
+        public void AddForm()
+        {
+            
+        }
+    }
+}

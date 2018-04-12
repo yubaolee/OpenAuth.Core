785cfcc533cb67bbbcc8b08f1c5d1601ef726ba0
yubao <yubaolee@163.com>
2018-04-13 07:10:02 +0800

    check bugs

---
+++ OpenAuth.App/Request/QueryResourcesReq.cs
@@ -0,0 +1,11 @@
+﻿namespace OpenAuth.App.Request
+{
+    public class QueryResourcesReq : PageReq
+    {
+        /// <summary>
+        /// TypeID
+        /// </summary>
+        public string TypeId { get; set; }
+
+    }
+}
---
+++ OpenAuth.Mvc/Controllers/ResourcesController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Mvc;
+using Infrastructure;
+using OpenAuth.App;
+using OpenAuth.App.Request;
+using OpenAuth.App.Response;
+using OpenAuth.Repository.Domain;
+
+namespace OpenAuth.Mvc.Controllers
+{
+    public class ResourcesController : BaseController
+    {
+        public ResourceApp App { get; set; }
+
+        //
+        // GET: /UserManager/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string All([FromUri]QueryResourcesReq request)
+        {
+            TableData data = new TableData();
+            data = App.All(request);
+            return JsonHelper.Instance.Serialize(data);
+        }
+
+        [System.Web.Mvc.HttpPost]
+        public string Delete(string[] ids)
+        {
+            Response resp = new Response();
+            try
+            {
+                App.Delete(ids);
+            }
+            catch (Exception e)
+            {
+                resp.Code = 500;
+                resp.Message = e.Message;
+            }
+            return JsonHelper.Instance.Serialize(resp);
+        }
+
+        [System.Web.Mvc.HttpPost]
+        public string Add(Resource obj)
+        {
+            Response resp = new Response();
+            try
+            {
+                App.Add(obj);
+            }
+            catch (Exception e)
+            {
+                resp.Code = 500;
+                resp.Message = e.Message;
+            }
+            return JsonHelper.Instance.Serialize(resp);
+        }
+
+        [System.Web.Mvc.HttpPost]
+        public string Update(Resource obj)
+        {
+            Response resp = new Response();
+            try
+            {
+                App.Update(obj);
+            }
+            catch (Exception e)
+            {
+                resp.Code = 500;
+                resp.Message = e.Message;
+            }
+            return JsonHelper.Instance.Serialize(resp);
+        }
+
+
+    }
+}

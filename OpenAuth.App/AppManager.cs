6c05096138b0b79dbaa1ef6c2720babda92452b0
yubaolee@163.com <yubaolee@163.com>
2018-04-13 17:25:49 +0800

    check bugs

---
+++ OpenAuth.App/AppManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenAuth.App.Request;
+using OpenAuth.App.Response;
+using OpenAuth.Repository.Domain;
+
+namespace OpenAuth.App
+{
+    /// <summary>
+    /// 分类管理
+    /// </summary>
+    public class AppManager : BaseApp<Application>
+    {
+        public void Add(Application Application)
+        {
+            if (string.IsNullOrEmpty(Application.Id))
+            {
+                Application.Id = Guid.NewGuid().ToString();
+            }
+            Repository.Add(Application);
+        }
+
+        public void Update(Application Application)
+        {
+            Repository.Update(u =>u.Id,Application);
+        }
+
+
+        public List<Application> GetList(QueryAppListReq request)
+        {
+            var applications =  UnitWork.Find<Application>(null) ;
+           
+            return applications.ToList();
+        }
+
+    }
+}
---
+++ OpenAuth.Mvc/Controllers/ApplicationsController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Mvc;
+using Infrastructure;
+using OpenAuth.App;
+using OpenAuth.App.Request;
+using OpenAuth.App.Response;
+using OpenAuth.Repository.Domain;
+
+namespace OpenAuth.Mvc.Controllers
+{
+    public class ApplicationsController : BaseController
+    {
+        public AppManager App { get; set; }
+
+
+        public string GetList([FromUri]QueryAppListReq request)
+        {
+            return JsonHelper.Instance.Serialize(App.GetList(request));
+        }
+
+        [System.Web.Mvc.HttpPost]
+        public string Delete(string[] ids)
+        {
+            Response resp = new Response();
+            try
+            {
+                App.Delete(ids);
+            }
+            catch (Exception e)
+            {
+                resp.Code = 500;
+                resp.Message = e.Message;
+            }
+            return JsonHelper.Instance.Serialize(resp);
+        }
+
+        [System.Web.Mvc.HttpPost]
+        public string Add(Application obj)
+        {
+            Response resp = new Response();
+            try
+            {
+                App.Add(obj);
+            }
+            catch (Exception e)
+            {
+                resp.Code = 500;
+                resp.Message = e.Message;
+            }
+            return JsonHelper.Instance.Serialize(resp);
+        }
+
+        [System.Web.Mvc.HttpPost]
+        public string Update(Application obj)
+        {
+            Response resp = new Response();
+            try
+            {
+                App.Update(obj);
+            }
+            catch (Exception e)
+            {
+                resp.Code = 500;
+                resp.Message = e.Message;
+            }
+            return JsonHelper.Instance.Serialize(resp);
+        }
+
+
+    }
+}

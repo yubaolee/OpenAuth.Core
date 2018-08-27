ba7e3ec37c7541880fc1a7e514d8a1436daba11c
yubao <yubaolee@163.com>
2018-08-27 17:31:19 +0800

    routine update

---
+++ OpenAuth.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using OpenAuth.App;
+using OpenAuth.App.Request;
+using OpenAuth.App.Response;
+using OpenAuth.Repository.Domain;
+using OpenAuth.WebApi.Filters;
+
+namespace OpenAuth.WebApi.Controllers
+{
+    /// <summary>
+    /// 用户操作
+    /// </summary>
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManagerApp _app;
+
+        [HttpGet]
+        public Response<UserView> Get(string id)
+        {
+            var result = new Response<UserView>();
+            try
+            {
+                result.Result = _app.Get(id);
+            }
+            catch (Exception ex)
+            {
+                result.Code = 500;
+                result.Message = ex.InnerException?.Message ?? ex.Message;
+            }
+
+            return result;
+        }
+
+        //添加或修改
+       [HttpPost]
+        public Response AddOrUpdate(UserView obj)
+        {
+            var result = new Response();
+            try
+            {
+                _app.AddOrUpdate(obj);
+            }
+            catch (Exception ex)
+            {
+                result.Code = 500;
+                result.Message = ex.InnerException?.Message ?? ex.Message;
+            }
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// 加载列表
+        /// </summary>
+        [HttpGet]
+       [TokenFilter]
+        public TableData Load([FromQuery]QueryUserListReq request)
+        {
+            return _app.Load(request);
+        }
+
+       [HttpPost]
+        public Response Delete(string[] ids)
+        {
+            var result = new Response();
+            try
+            {
+                _app.Delete(ids);
+
+            }
+            catch (Exception ex)
+            {
+                result.Code = 500;
+                result.Message = ex.InnerException?.Message ?? ex.Message;
+            }
+
+            return result;
+        }
+
+        public UsersController(UserManagerApp app) 
+        {
+            _app = app;
+        }
+    }
+}

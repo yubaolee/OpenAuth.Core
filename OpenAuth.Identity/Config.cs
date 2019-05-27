2b195271d957f0d2655593fb171648c2c4130b64
yubaolee <yubaolee@163.com>
2019-05-27 23:16:54 +0800

    identity server

---
+++ OpenAuth.Identity/Config.cs
@@ -0,0 +1,68 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenAuth.IdentityServer
+{
+    public static class Config
+    {
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "admin",
+                    Password = "admin"
+                }
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+        /// <summary>
+        /// API信息
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new[]
+            {
+                new ApiResource("swagger_api", "OpenAuth.WebApi")
+            };
+        }
+        /// <summary>
+        /// 客服端信息
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "swagger_client",//客服端名称
+                    ClientName = "OpenAuth.WebApi",//客户端描述
+                    AllowedGrantTypes = GrantTypes.Implicit,//Implicit 方式
+                    AllowAccessTokensViaBrowser = true,//是否通过浏览器为此客户端传输访问令牌
+                    RedirectUris =
+                    {
+                        "http://localhost:52789/swagger/oauth2-redirect.html"
+                    },
+                    AllowedScopes = { "swagger_api" }
+                }
+            };
+        }
+    }
+}
---
+++ OpenAuth.Identity/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace OpenAuth.IdentityServer
+{
+    public class Startup
+    {
+        public IHostingEnvironment Environment { get; }
+
+        public Startup(IHostingEnvironment environment)
+        {
+            Environment = environment;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            var builder = services.AddIdentityServer()
+                    .AddInMemoryIdentityResources(Config.GetIdentityResources())
+                    .AddInMemoryApiResources(Config.GetApis())
+                    .AddInMemoryClients(Config.GetClients())
+                    .AddTestUsers(Config.GetUsers());
+
+            if (Environment.IsDevelopment())
+            {
+                builder.AddDeveloperSigningCredential();
+            }
+            else
+            {
+                throw new Exception("need to configure key material");
+            }
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+            app.UseIdentityServer();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}

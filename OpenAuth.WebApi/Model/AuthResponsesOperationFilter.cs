0ad5edbf43b70897e56a7c146ac98ee9997d1c43
ÂëÉñ <dotcpp@163.com>
2019-06-28 14:23:56 +0800

    fix:swagger添加oauth2的条件

--- OpenAuth.WebApi/Model/AuthResponsesOperationFilter.cs
+++ OpenAuth.WebApi/Model/AuthResponsesOperationFilter.cs
@@ -1,21 +1,36 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+using OpenAuth.App;
 using Swashbuckle.AspNetCore.Swagger;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace OpenAuth.WebApi.Model
 {
+    /// <summary>
+    /// swagger请求的时候，如果是Identity方式，自动加授权方式
+    /// </summary>
     public class AuthResponsesOperationFilter : IOperationFilter
     {
+        private IOptions<AppSetting> _appConfiguration;
+
+        public AuthResponsesOperationFilter(IOptions<AppSetting> appConfiguration)
+        {
+            _appConfiguration = appConfiguration;
+        }
+
         public void Apply(Operation operation, OperationFilterContext context)
         {
-            //获取是否添加登录特性
-            var authAttributes = context.MethodInfo.DeclaringType.GetCustomAttributes(true)
-             .Union(context.MethodInfo.GetCustomAttributes(true))
-             .OfType<AuthorizeAttribute>().Any();
+            if (!_appConfiguration.Value.IsIdentityAuth)
+            {
+                return;
+            }
 
-            if (authAttributes)
+            var anonymous = context.MethodInfo.DeclaringType.GetCustomAttributes(true)
+                .Union(context.MethodInfo.GetCustomAttributes(true))
+                .OfType<AllowAnonymousAttribute>().Any();
+            if (!anonymous)
             {
                 //operation.Responses.Add("401", new Response { Description = "暂无访问权限" });
                 //operation.Responses.Add("403", new Response { Description = "禁止访问" });

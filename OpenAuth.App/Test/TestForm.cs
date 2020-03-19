ce8778a61256689235d84feac4f7c611f5458cb3
yubaolee <yubaolee@163.com>
2020-03-19 20:15:37 +0800

    升级单元测试框架； 添加表单单元测试

---
+++ OpenAuth.App/Test/TestForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Infrastructure;
+using Infrastructure.Cache;
+using Microsoft.AspNetCore.Http;
+using NUnit.Framework;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using OpenAuth.App.Request;
+using OpenAuth.App.SSO;
+
+namespace OpenAuth.App.Test
+{
+    [TestFixture]
+    public class TestForm :TestBase
+    {
+        public override ServiceCollection GetService()
+        {
+            var services = new ServiceCollection();
+
+            var cachemock = new Mock<ICacheContext>();
+            cachemock.Setup(x => x.Get<UserAuthSession>("tokentest")).Returns(new UserAuthSession { Account = "admin" });
+            services.AddScoped(x => cachemock.Object);
+
+            var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+            httpContextAccessorMock.Setup(x => x.HttpContext.Request.Query[Define.TOKEN_NAME]).Returns("tokentest");
+
+            services.AddScoped(x => httpContextAccessorMock.Object);
+
+            return services;
+        }
+
+
+        [Test]
+        public void Load()
+        {
+            var app = _autofacServiceProvider.GetService<FormApp>();
+            var result = app.Load(new QueryFormListReq
+            {
+                page = 1,
+                limit = 10
+            });
+            Console.WriteLine(JsonHelper.Instance.Serialize(result));
+        }
+    }
+}

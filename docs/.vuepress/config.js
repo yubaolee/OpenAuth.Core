f4a16eda1ac24250f199689adb18ee6657ec4444
yubaolee <yubaolee@163.com>
2020-05-07 19:59:47 +0800

    添加首页

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    title: 'OpenAuth.Core文档--最好用的.NET权限工作流框架',
-    description: 'OpenAuth.Core文档,最好用的.net开源工作流框架,最好用的.net core vue element-ui前后端分离框架'
+    title: 'OpenAuth.Core',
+    description: '最好用的.net权限工作流框架,最好用的.net core vue前后分离框架'
     ,themeConfig : {
       nav : [
           { text: 'OpenAuth.Core', link: '/core/' },

d77e95785613bae18486d725c0a43b19a3012b80
yubaolee <yubaolee@163.com>
2020-05-30 23:02:51 +0800

    fix bug #I1HEL8 完美方案应该用WebApplicationFactory，但不能mock，需要跟进

--- OpenAuth.WebApi/Test/CheckControllerTest.cs
+++ OpenAuth.WebApi/Test/CheckControllerTest.cs
@@ -1,109 +1,70 @@
-﻿using System;
-using Autofac.Extensions.DependencyInjection;
+﻿// <copyright file="UserSessionControllerTest.cs" company="OpenAuth.Mvc">
+//     Copyright (c) http://www.openauth.me. All rights reserved.
+// </copyright>
+// <summary>
+// 测试加载用户菜单
+// </summary>
+// ***********************************************************************
+
+using System;
+using System.Diagnostics;
 using Infrastructure;
 using Infrastructure.Cache;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.EntityFrameworkCore;
-using NUnit.Framework;
-using OpenAuth.App.SSO;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Moq;
-using Newtonsoft.Json;
+using NUnit.Framework;
 using OpenAuth.App;
-using OpenAuth.Repository;
+using OpenAuth.App.SSO;
+using OpenAuth.App.Test;
 using OpenAuth.WebApi.Controllers;
-using OpenAuth.WebApi.Model;
 
 namespace OpenAuth.WebApi.Test
 {
-    public class CheckControllerTest
+    public class CheckControllerTest : TestBase
     {
-        private CheckController _controller;
-        private ILogger _logger;
-        private IHost _host;
 
-        [SetUp]
-        public void Init()
+        //如果要测试controller，则要重写
+        public override ServiceCollection GetService()
         {
+            var services = new ServiceCollection();
+
             var cachemock = new Mock<ICacheContext>();
-            cachemock.Setup(x => x.Get<UserAuthSession>("token")).Returns(new UserAuthSession { Account = "admin" });
+            cachemock.Setup(x => x.Get<UserAuthSession>("tokentest")).Returns(new UserAuthSession{Account = "admin"});
+            services.AddScoped(x => cachemock.Object);
 
             var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
-            httpContextAccessorMock.Setup(x => x.HttpContext.Request.Query[Define.TOKEN_NAME]).Returns("token");
+            httpContextAccessorMock.Setup(x => x.HttpContext.Request.Query[Define.TOKEN_NAME]).Returns("tokentest");
 
+            services.AddScoped(x => httpContextAccessorMock.Object);
 
-            var hostBuilder = Host.CreateDefaultBuilder()
-                .ConfigureWebHostDefaults(webHostBuilder =>
-                {
-                    webHostBuilder.UseStartup<Startup>();
-                    webHostBuilder.UseTestServer();
-                    webHostBuilder.UseEnvironment("Development");
-                  //  启动时调用了webapi的Startup ConfigureContainer造成下面的mock没起作用
-                  //  webHostBuilder.ConfigureTestContainer();
+            services.AddMvc().AddControllersAsServices();
+            services.AddScoped<CheckController>();
 
-                    webHostBuilder.ConfigureTestServices((services) =>
-                    {
-                        services.RemoveAll(typeof(IHostedService));
-                        services.AddScoped(x => cachemock.Object);
-                        services.AddScoped(x => httpContextAccessorMock.Object);
-                        AutofacExt.InitForTest(services);
-                    });
-                })
-                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-                .ConfigureLogging((hostingContext, logging) =>
-                {
-                    logging.ClearProviders(); //去掉默认的日志
-                    logging.AddFilter("System", LogLevel.Warning);
-                    logging.AddFilter("Microsoft", LogLevel.Warning);
-                    logging.AddLog4Net(); //默认的appsettings.json配置是warn级别，info是看不到的
-                }).UseEnvironment("Development");
-//                .ConfigureServices(u =>
-//                {
-//                    u.AddScoped(x => cachemock.Object);
-//                    u.AddScoped(x => httpContextAccessorMock.Object);
-//                    AutofacExt.InitForTest(u);
-//
-//                });
-            
-            
-            // Build and start the IHost
-            _host = hostBuilder.Start();
-            
-//          获取不到controller，但可以获取到其他注入的对象？ 
-//           _controller = _host.GetTestServer().Services.GetRequiredService<CheckController>();
-            _logger = _host.GetTestServer().Services.GetService<ILogger<CheckControllerTest>>();
+            //读取配置文件
+            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            // Duplicate here any configuration sources you use.
+            configurationBuilder.AddJsonFile("AppSettings.json");
+            IConfiguration configuration = configurationBuilder.Build();
+            services.Configure<AppSetting>(configuration.GetSection("AppSetting"));
+
+            services.AddLogging();
+
+            return services;
         }
+
 
         [Test]
-        public void LoadModuleTree()
+        public void GetModulesTree()
         {
-            // Create an HttpClient to send requests to the TestServer
-            var client = _host.GetTestClient();
+            Stopwatch watch = Stopwatch.StartNew();
+            var controller = _autofacServiceProvider.GetService<CheckController>();
 
-            var result = client.GetStringAsync("api/Check/GetModulesTree").Result;
-            
-            Console.WriteLine(result); 
+            var result =  controller.GetModulesTree();
+            Console.WriteLine(JsonHelper.Instance.Serialize(result));
+            watch.Stop();
+            Console.WriteLine($"总耗时:{watch.ElapsedMilliseconds}");
         }
-
-
-//        [Test]
-//        public void Login()
-//        {
-//
-//            var result = _controller.Login(new PassportLoginRequest
-//            {
-//                Account = Define.SYSTEM_USERNAME,
-//                AppKey = "openauth",
-//                Password = Define.SYSTEM_USERPWD
-//            });
-//
-//            _logger.LogInformation(JsonHelper.Instance.Serialize(result));
-//        }
     }
 }

14b0eaa0a8ef67ce1b07953807ed26332aebd77a
yubaolee <yubaolee@163.com>
2020-06-14 00:19:46 +0800

    fix _logApp有时为空的异常

--- OpenAuth.WebApi/Model/OpenAuthFilter.cs
+++ OpenAuth.WebApi/Model/OpenAuthFilter.cs
@@ -44,8 +44,8 @@ namespace OpenAuth.WebApi.Model
                     Message = "认证失败，请提供认证信息"
                 });
             }
-
-            _logApp.Add(new SysLog
+            //todo:这个地方有个很奇怪的错误，_logApp有时会为空
+            _logApp?.Add(new SysLog
             {
                 Content = $"用户访问",
                 Href = $"{Controllername}/{Actionname}",

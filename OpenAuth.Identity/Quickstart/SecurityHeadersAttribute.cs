fac9efec482f132a7b1d87b7473a7b5a17a1b88f
yubaolee <yubaolee@163.com>
2020-09-01 00:54:07 +0800

    fix SameSite，see detail:https://www.thinktecture.com/en/identity/samesite/prepare-your-identityserver/

--- OpenAuth.Identity/Quickstart/SecurityHeadersAttribute.cs
+++ OpenAuth.Identity/Quickstart/SecurityHeadersAttribute.cs
@@ -15,16 +15,16 @@ namespace OpenAuth.IdentityServer.Quickstart
             if (result is ViewResult)
             {
                 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
-                if (!context.HttpContext.Response.Headers.ContainsKey("X-Content-Type-Options"))
-                {
-                    context.HttpContext.Response.Headers.Add("X-Content-Type-Options", "nosniff");
-                }
-
-                // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
-                if (!context.HttpContext.Response.Headers.ContainsKey("X-Frame-Options"))
-                {
-                    context.HttpContext.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
-                }
+                // if (!context.HttpContext.Response.Headers.ContainsKey("X-Content-Type-Options"))
+                // {
+                //     context.HttpContext.Response.Headers.Add("X-Content-Type-Options", "nosniff");
+                // }
+                //
+                // // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+                // if (!context.HttpContext.Response.Headers.ContainsKey("X-Frame-Options"))
+                // {
+                //     context.HttpContext.Response.Headers.Add("X-Frame-Options", "SAMEORIGIN");
+                // }
 
                 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
                 var csp = "default-src 'self'; object-src 'none'; frame-ancestors 'none'; sandbox allow-forms allow-same-origin allow-scripts; base-uri 'self';";
@@ -34,22 +34,22 @@ namespace OpenAuth.IdentityServer.Quickstart
                 // csp += "img-src 'self' https://pbs.twimg.com;";
 
                 // once for standards compliant browsers
-                if (!context.HttpContext.Response.Headers.ContainsKey("Content-Security-Policy"))
-                {
-                    context.HttpContext.Response.Headers.Add("Content-Security-Policy", csp);
-                }
-                // and once again for IE
-                if (!context.HttpContext.Response.Headers.ContainsKey("X-Content-Security-Policy"))
-                {
-                    context.HttpContext.Response.Headers.Add("X-Content-Security-Policy", csp);
-                }
+                // if (!context.HttpContext.Response.Headers.ContainsKey("Content-Security-Policy"))
+                // {
+                //     context.HttpContext.Response.Headers.Add("Content-Security-Policy", csp);
+                // }
+                // // and once again for IE
+                // if (!context.HttpContext.Response.Headers.ContainsKey("X-Content-Security-Policy"))
+                // {
+                //     context.HttpContext.Response.Headers.Add("X-Content-Security-Policy", csp);
+                // }
 
                 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
                 var referrer_policy = "no-referrer";
-                if (!context.HttpContext.Response.Headers.ContainsKey("Referrer-Policy"))
-                {
-                    context.HttpContext.Response.Headers.Add("Referrer-Policy", referrer_policy);
-                }
+                // if (!context.HttpContext.Response.Headers.ContainsKey("Referrer-Policy"))
+                // {
+                //     context.HttpContext.Response.Headers.Add("Referrer-Policy", referrer_policy);
+                // }
             }
         }
     }
--- OpenAuth.Identity/Startup.cs
+++ OpenAuth.Identity/Startup.cs
@@ -4,6 +4,7 @@
 
 using Autofac;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,7 +33,9 @@ namespace OpenAuth.IdentityServer
                 .AddInMemoryApiResources(Config.GetApis())
                 .AddInMemoryClients(Config.GetClients(Environment.IsProduction()))
                 .AddProfileService<CustomProfileService>();
-
+            
+            services.ConfigureNonBreakingSameSiteCookies();
+            
             services.AddCors();
 //          todo:如果正式 环境请用下面的方式限制随意访问跨域
 //            var origins = new []
@@ -92,6 +95,9 @@ namespace OpenAuth.IdentityServer
             {
                 app.UseDeveloperExceptionPage();
             }
+            
+            
+            app.UseCookiePolicy();
             
             //todo:测试可以允许任意跨域，正式环境要加权限
             app.UseCors(builder => builder.AllowAnyOrigin()

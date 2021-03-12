0878e478b11ff1bc6af1e1a1adfbc27dd10a6ac4
yubaolee <yubaolee@163.com>
2021-03-13 00:00:10 +0800

    增加雪花算法，等原作者提交PR

---
+++ Infrastructure/Snowflake/ISnowWorker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yitter.IdGenerator
+{
+    internal interface ISnowWorker
+    {
+        Action<OverCostActionArg> GenAction { get; set; }
+
+        long NextId();
+    }
+}

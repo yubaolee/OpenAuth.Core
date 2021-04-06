a3695cdb80cf5d13fe28e7c03cb1287348280831
yubaolee <yubaolee@163.com>
2021-04-06 15:53:11 +0800

    增加启动LOGO

--- OpenAuth.WebApi/Program.cs
+++ OpenAuth.WebApi/Program.cs
@@ -1,4 +1,5 @@
-﻿using Autofac.Extensions.DependencyInjection;
+﻿using System;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -9,6 +10,21 @@ namespace OpenAuth.WebApi
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine(@"
+               ____                                 _   _       _   _      _   
+              / __ \                     /\        | | | |     | \ | |    | |  
+             | |  | |_ __   ___ _ __    /  \  _   _| |_| |__   |  \| | ___| |_ 
+             | |  | | '_ \ / _ \ '_ \  / /\ \| | | | __| '_ \  | . ` |/ _ \ __|
+             | |__| | |_) |  __/ | | |/ ____ \ |_| | |_| | | |_| |\  |  __/ |_ 
+              \____/| .__/ \___|_| |_/_/    \_\__,_|\__|_| |_(_)_| \_|\___|\__|
+                    | |                                                        
+                    |_|                                                        
+            -------------------------------------------------------------------
+            Author           :  yubaolee
+            .Net 5 Repository:  https://gitee.com/dotnetchina/OpenAuth.Net
+            .Net core 3.1    :  https://gitee.com/yubaolee/OpenAuth.Core
+            -------------------------------------------------------------------
+            ");
             CreateHostBuilder(args).Build().Run();
         }
 

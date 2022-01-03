54963725ccdf8e7d3d02887286f3e90d80642e1e
yubaolee <yubaolee@163.com>
2022-01-03 22:24:25 +0800

    更新文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,111 +1,70 @@
 module.exports = {
-    title: 'OpenAuth.Core'
-    ,description: '最好用的.net权限工作流框架,最好用的.net core vue前后分离框架'
-    ,head: [
-      ['link', { rel: 'icon', href: '/logo.png' }]
-    ]
-    ,themeConfig : {
-      lastUpdated: '最后更新时间', // string | boolean
-      nav : [
-        { text: '首页', link: 'http://openauth.net.cn/' , target:'_blank' },
-          { text: 'OpenAuth.Core(.Net Core/.Net 5)', link: '/core/' },
-          { text: 'OpenAuth.Net(.Net 4.5)', link: '/net/' },
-          {
-            text: '项目地址',
-            ariaLabel: 'Proj Menu',
-            items: [
-              { text: 'gitee', link: 'https://gitee.com/yubaolee/OpenAuth.Core' , target:'_blank' },
-              { text: 'github', link: 'https://github.com/yubaolee/OpenAuth.Core' , target:'_blank' }
-            ]
-          }
-      ],
-      sidebar: {
-        '/core/': [
-          ['','项目介绍']
-          ,{
-            title: '后端开发',   // 必要的
-            path: 'start',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
-            sidebarDepth: 1,    // 可选的, 默认值是 1
-            collapsable: false, 
-            children: [
-              "start"
-              ,"specialist"
-              ,"deploy" 
-              ,"deployapi" 
-              ,"devnew"
-              ,"multidbs"
-              ,"multitenant"
-              ,"unitwork"
-              ,"entity"
-              ,"datavalidation"
-              ,"log"
-              ,"identity" 
-              ,"job"
-              ,"cache"
-              ,"unittest"
-            ]
-          }
-          ,{
-            title: '权限控制',   // 必要的
-            sidebarDepth: 1,    // 可选的, 默认值是 1
-            collapsable: false, 
-            children: [
-              'apiauth'
-              ,'logininfo'
-              ,'dataprivilege'
-              ,'datapropertyrule'
-              
-            ]
-          }
-          ,"form" 
-          ,{
-            title: '工作流',   // 必要的
-            sidebarDepth: 1,    // 可选的, 默认值是 1
-            collapsable: false, 
-            children: [
-              'flowinstance',
-              'flowinstancestatus',
-              'thirdparty'
-              
-            ]
-          }
-          ,{
-            title: '前端开发',   // 必要的
-            sidebarDepth: 1,    // 可选的, 默认值是 1
-            collapsable: false, 
-            children: [
-              'wwwarchitect'
-            ]
-          }
-          ,{
-            title: '更新日志',   // 必要的
-            sidebarDepth: 1,    // 可选的, 默认值是 1
-            collapsable: false, 
-            children: [
-              'changelog'
-              ,'routineupdate'
-            ]
-          }
-          ,"faq" 
-        ],
-
-        '/pro/': [
-          ['','项目介绍']
-          ,"deploy"
-          ,"structure"
-          ,"devnew"
-          ,"datapropertyrule"
-          ,"form"
-          ,"router"
-          ,"components"
-        ],
-  
-        '/net/': [
-          ['','项目介绍']
-          ,"start"  
-          ,"deploy" 
-          ,"devnew"
+  title: 'OpenAuth.Net',
+  description: '最好用的.net权限工作流框架,最好用的.net vue前后分离框架',
+  head: [['link', { rel: 'icon', href: '/logo.png' }]],
+  themeConfig: {
+    lastUpdated: '最后更新时间', // string | boolean
+    nav: [
+      { text: '首页', link: 'http://openauth.net.cn/', target: '_blank' },
+      {
+        text: '在线文档',
+        ariaLabel: 'Proj Menu',
+        items: [
+          { text: 'OpenAuth.Net(.Net Core/.Net 5)', link: '/core/' , target: '_blank'},
+          { text: 'OpenAuth.Pro(企业版)', link: '/pro/' , target: '_blank'},
+          { text: '.Net 4.5版本文档', link: '/net/' , target: '_blank'}
+        ]
+      },
+      {
+        text: '项目地址',
+        ariaLabel: 'Proj Menu',
+        items: [
+          { text: 'gitee', link: 'https://gitee.com/yubaolee/OpenAuth.Core', target: '_blank' },
+          { text: 'github', link: 'https://github.com/yubaolee/OpenAuth.Core', target: '_blank' }
         ]
       }
+    ],
+    sidebar: {
+      '/core/': [
+        ['', '项目介绍'],
+        {
+          title: '后端开发', // 必要的
+          path: 'start', // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+          sidebarDepth: 1, // 可选的, 默认值是 1
+          collapsable: false,
+          children: ['start', 'specialist', 'deploy', 'deployapi', 'devnew', 'multidbs', 'multitenant', 'unitwork', 'entity', 'datavalidation', 'log', 'identity', 'job', 'cache', 'unittest']
+        },
+        {
+          title: '权限控制', // 必要的
+          sidebarDepth: 1, // 可选的, 默认值是 1
+          collapsable: false,
+          children: ['apiauth', 'logininfo', 'dataprivilege', 'datapropertyrule']
+        },
+        'form',
+        {
+          title: '工作流', // 必要的
+          sidebarDepth: 1, // 可选的, 默认值是 1
+          collapsable: false,
+          children: ['flowinstance', 'flowinstancestatus', 'thirdparty']
+        },
+        {
+          title: '前端开发', // 必要的
+          sidebarDepth: 1, // 可选的, 默认值是 1
+          collapsable: false,
+          children: ['wwwarchitect']
+        },
+        {
+          title: '更新日志', // 必要的
+          sidebarDepth: 1, // 可选的, 默认值是 1
+          collapsable: false,
+          children: ['changelog', 'routineupdate']
+        },
+        'faq'
+      ],
+
+      '/pro/': [['', '项目介绍'], 'deploy', 'structure', 'devnew', 'datapropertyrule', 'form', 'router', 'components'],
+
+      '/net/': [['', '项目介绍'], 'start', 'deploy', 'devnew']
+    }
   }
 }
--- docs/core/README.md
+++ docs/core/README.md
@@ -1,6 +1,18 @@
 ![LOGO](/logocore.png "1.png")
 
-OpenAuth.Core是基于 **.Net Core/.Net 5** 的开源权限工作流快速开发框架。框架汲取Martin Fowler企业级应用开发思想及全新技术组合（IdentityServer、EF core、Quartz、AutoFac、WebAPI、Swagger、Mock、NUnit、VUE、Element-ui等），核心模块包括：组织机构、角色用户、权限授权、表单设计、工作流等。
+OpenAuth.Net是基于 **.Net Core/.Net 5** 的开源权限工作流快速开发框架。框架汲取Martin Fowler企业级应用开发思想及全新技术组合（IdentityServer、EF core、Quartz、AutoFac、WebAPI、Swagger、Mock、NUnit、VUE、Element-ui等），核心模块包括：组织机构、角色用户、权限授权、表单设计、工作流等。
+
+::: tip 提示
+
+gitee上面两个版本，仅SDK的版本不同，代码完全相同。其中：
+
+* [OpenAuth.Net](https://gitee.com/dotnetchina/OpenAuth.Net) 的SDK版本为5.0.100，推荐使用该版本
+
+* [OpenAuth.Core](https://gitee.com/yubaolee/OpenAuth.Core) 的SDK版本为.Net Core 3.1.100，历史原因暂且保留，未来的某天会关闭它
+
+.Net目前SDK升级特别方便，只需花费3分钟时间既可升级到.Net 6/7/8...等。请参考：[3分钟的时间把.net core 3.1的升级到.NET 5](https://www.cnblogs.com/yubaolee/p/Net3ToNet5.html)，所以不要纠结SDK版本问题。
+:::
+
 
 ## 技术栈
 

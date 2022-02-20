ac0dfef321cb7706b56fa6a9a326f27acdd19942
yubaolee <yubaolee@163.com>
2022-02-21 00:39:55 +0800

    fix webapi 未设置代码生成 Load加载数据报错，MVC页面没有代码生成界面 https://gitee.com/yubaolee/OpenAuth.Core/issues/I4RLEP

--- OpenAuth.Repository/Test/TestUnitWork.cs
+++ OpenAuth.Repository/Test/TestUnitWork.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Data;
 using System.Linq;
 using Infrastructure;
+using Microsoft.Data.SqlClient;
 using NUnit.Framework;
 using Microsoft.Extensions.DependencyInjection;
+using MySqlConnector;
 using OpenAuth.Repository.Domain;
 using OpenAuth.Repository.Interface;
 
@@ -21,6 +24,20 @@ namespace OpenAuth.Repository.Test
         {
             var unitWork = _autofacServiceProvider.GetService<IUnitWork<OpenAuthDBContext>>();
             var users = unitWork.ExecProcedure<User>("sp_alluser");
+            Console.WriteLine(JsonHelper.Instance.Serialize(users));
+        }
+        
+        
+        /// <summary>
+        /// 测试Mysql执行存储过程
+        /// </summary>
+        [Test]
+        public void ExecProcedureWithParam()
+        {
+            var unitWork = _autofacServiceProvider.GetService<IUnitWork<OpenAuthDBContext>>();
+            var param = new MySqlParameter("keyword", SqlDbType.NVarChar);
+            param.Value = "test%";
+            var users = unitWork.ExecProcedure<User>("sp_alluser", new []{param});
             Console.WriteLine(JsonHelper.Instance.Serialize(users));
         }
         

717ed2c042d78188734b0f05bbc93dff583ef8f9
yubaolee <yubaolee@163.com>
2022-02-22 22:22:22 +0800

    增加拖拽表单设计说明

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,32 +1,60 @@
 module.exports = {
   title: 'OpenAuth.Net',
   description: '最好用的.net权限工作流框架,最好用的.net vue前后分离框架',
-  head: [['link', { rel: 'icon', href: '/logo.png' }]],
-  extendMarkdown: md => {
-    md.set({ breaks: true })
+  head: [
+    ['link', {
+      rel: 'icon',
+      href: '/logo.png'
+    }]
+  ],
+  extendMarkdown: (md) => {
+    md.set({
+      breaks: true
+    })
     md.use(require('markdown-it-plantuml'))
   },
   themeConfig: {
     lastUpdated: '最后更新时间', // string | boolean
-    nav: [
-      { text: '首页', link: 'http://openauth.net.cn/', target: '_blank' },
+    nav: [{
+        text: '首页',
+        link: 'http://openauth.net.cn/',
+        target: '_blank'
+      },
       {
         text: '在线文档',
         ariaLabel: 'Proj Menu',
-        items: [
-          { text: 'OpenAuth.Net(.Net Core/.Net 5)', link: '/core/' , target: '_blank'},
-          { text: 'OpenAuth.Pro(企业版)', link: '/pro/' , target: '_blank'},
-          { text: '.Net 4.5版本文档', link: '/net/' , target: '_blank'}
-        ]
+        items: [{
+            text: 'OpenAuth.Net(.Net Core/.Net 5)',
+            link: '/core/',
+            target: '_blank'
+          },
+          {
+            text: 'OpenAuth.Pro(企业版)',
+            link: '/pro/',
+            target: '_blank'
+          },
+          {
+            text: '.Net 4.5版本文档',
+            link: '/net/',
+            target: '_blank'
+          },
+        ],
       },
       {
         text: '项目地址',
         ariaLabel: 'Proj Menu',
-        items: [
-          { text: 'gitee', link: 'https://gitee.com/yubaolee/OpenAuth.Core', target: '_blank' },
-          { text: 'github', link: 'https://github.com/yubaolee/OpenAuth.Core', target: '_blank' }
-        ]
-      }
+        items: [{
+            text: 'gitee',
+            link: 'https://gitee.com/yubaolee/OpenAuth.Core',
+            target: '_blank'
+          },
+          {
+            text: 'github',
+            link: 'https://github.com/yubaolee/OpenAuth.Core',
+            target: '_blank'
+          },
+        ],
+      },
     ],
     sidebar: {
       '/core/': [
@@ -36,39 +64,62 @@ module.exports = {
           path: 'start', // 可选的, 标题的跳转链接，应为绝对路径且必须存在
           sidebarDepth: 1, // 可选的, 默认值是 1
           collapsable: false,
-          children: ['start', 'specialist', 'deploy', 'deployapi', 'devnew', 'multidbs', 'multitenant', 'unitwork', 'entity', 'datavalidation', 'log', 'identity', 'job', 'cache', 'unittest']
+          children: ['start', 'specialist', 'deploy', 'deployapi', 'devnew', 'multidbs', 'multitenant', 'unitwork', 'entity', 'datavalidation', 'log', 'identity', 'job', 'cache', 'unittest'],
         },
         {
           title: '权限控制', // 必要的
           sidebarDepth: 1, // 可选的, 默认值是 1
           collapsable: false,
-          children: ['apiauth', 'logininfo', 'dataprivilege', 'datapropertyrule']
+          children: ['apiauth', 'logininfo', 'dataprivilege', 'datapropertyrule'],
         },
         'form',
         {
           title: '工作流', // 必要的
           sidebarDepth: 1, // 可选的, 默认值是 1
           collapsable: false,
-          children: ['flowinstance', 'flowinstancestatus', 'thirdparty']
+          children: ['flowinstance', 'flowinstancestatus', 'thirdparty'],
         },
         {
           title: '前端开发', // 必要的
           sidebarDepth: 1, // 可选的, 默认值是 1
           collapsable: false,
-          children: ['wwwarchitect']
+          children: ['wwwarchitect'],
         },
         {
           title: '更新日志', // 必要的
           sidebarDepth: 1, // 可选的, 默认值是 1
           collapsable: false,
-          children: ['changelog', 'routineupdate']
+          children: ['changelog', 'routineupdate'],
         },
-        'faq'
+        'faq',
       ],
 
-      '/pro/': [['', '项目介绍'], 'deploy', 'structure', 'devnew', 'datapropertyrule', 'form', 'router', 'components','faq'],
+      '/pro/': [
+        ['', '项目介绍'],
+        'deploy',
+        'structure',
+        'devnew',
+        'datapropertyrule',
+        {
+          title: '表单设计', // 必要的
+          sidebarDepth: 1, // 可选的, 默认值是 1
+          collapsable: false,
+          children: ['dragform'],
+        },
+        {
+          title: '工作流', // 必要的
+          sidebarDepth: 1, // 可选的, 默认值是 1
+          collapsable: false,
+          children: ['form'],
+        },
+        'router',
+        'components',
+        'faq',
+      ],
 
-      '/net/': [['', '项目介绍'], 'start', 'deploy', 'devnew']
-    }
-  }
+      '/net/': [
+        ['', '项目介绍'], 'start', 'deploy', 'devnew'
+      ],
+    },
+  },
 }

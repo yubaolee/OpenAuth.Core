bddf2d4f645a178d16ea656ad10619443d63baa9
yubaolee <yubaolee@163.com>
2023-08-30 13:51:37 +0800

    fix issue #I7X427 vs2019打开OpenAuth.Net 6.0异常

--- OpenAuth.Repository/SqlSugarRepository.cs
+++ OpenAuth.Repository/SqlSugarRepository.cs
@@ -1,16 +1,17 @@
-﻿using System.Collections.Generic;
-using SqlSugar;
+﻿using SqlSugar;
 
-namespace OpenAuth.Repository;
-
-/// <summary>
-/// SqlSugar仓储
-/// <para>具体用法参考：https://www.donet5.com/Home/Doc?typeId=1228</para>
-/// </summary>
-public class SqlSugarRepository<T>: SimpleClient<T> where T : class, new()
+namespace OpenAuth.Repository
 {
-    public SqlSugarRepository(ISqlSugarClient client)
-    {          
-        base.Context=client; 
+    /// <summary>
+    /// SqlSugar仓储
+    /// <para>具体用法参考：https://www.donet5.com/Home/Doc?typeId=1228</para>
+    /// </summary>
+    public class SqlSugarRepository<T> : SimpleClient<T> where T : class, new()
+    {
+        public SqlSugarRepository(ISqlSugarClient client)
+        {
+            base.Context = client;
+        }
     }
 }
+
